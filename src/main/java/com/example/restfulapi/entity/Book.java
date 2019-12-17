d6575b94e09fb1015cb1d380112dc373842474cb
liguangpeng <liguangpeng@yqzbw.com>
2019-12-17 20:07:15 +0800

    first commit

---
+++ src/main/java/com/example/restfulapi/entity/Book.java
@@ -0,0 +1,27 @@
+package com.example.restfulapi.entity;/*
+ * @program: restful-api
+ *
+ * @description:
+ *
+ * @author: guangpeng.li
+ *
+ * @create: 2019-12-17 19:19
+ */
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@RequiredArgsConstructor
+public class Book {
+    @Id
+    private String id;
+    //书名
+    private String name;
+    private String author;
+    private String description;
+    private boolean status;
+}
