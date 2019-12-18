54c53da8cb23f3622edfc055028e70a3f50eb691
liguangpeng <liguangpeng@yqzbw.com>
2019-12-18 20:00:57 +0800

    feat: book api

--- src/main/java/com/example/restfulapi/controller/BookController.java
+++ src/main/java/com/example/restfulapi/controller/BookController.java
@@ -9,16 +9,17 @@ package com.example.restfulapi.controller;
  * @create: 2019-12-17 20:21
  */
 
+import com.example.restfulapi.entity.Book;
 import com.example.restfulapi.repo.BookRepository;
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * rest 风格 api
@@ -42,6 +43,7 @@ public class BookController {
     /**
      * 获取所有书单
      * GET /api/v1/books
+     * GET /api/v1/books/{id}
      *
      * @return http 响应
      */
@@ -50,9 +52,52 @@ public class BookController {
         return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
     }
 
+    /**
+     * 获取一个书单
+     *
+     * @param id id
+     * @return http响应
+     */
     @GetMapping("/books/{id}")
     public HttpEntity<?> booksOne(@PathVariable String id) {
         return new ResponseEntity<>(bookRepository.findById(new ObjectId(id)), HttpStatus.OK);
     }
 
+    /**
+     * 添加一个书单
+     * POST /api/v1/books
+     *
+     * @param book       书单
+     * @param bindResult 验证
+     * @return http响应
+     */
+    @PostMapping("/books")
+    public HttpEntity<?> booksAdd(@Valid @RequestBody Book book, BindingResult bindResult) {
+
+        book.setId(null);
+        return new ResponseEntity<>(bookRepository.save(book), HttpStatus.CREATED);
+    }
+
+    /**
+     * 更新一条书单，提供全部信息
+     * PUT /api/v1/books/{id}
+     *
+     * @param id            更新数据id
+     * @param book          更新后的书单
+     * @param bindingResult 绑定结果
+     * @return http 响应
+     */
+    @PutMapping("/books/{id}")
+    public HttpEntity<?> booksPut(@Valid @PathVariable String id, @RequestBody Book book, BindingResult bindingResult) {
+        Book save;
+        Optional<Book> existBook = bookRepository.findById(new ObjectId(id));
+        if (existBook.isPresent()) {
+            book.setId(existBook.get().getId());
+            save = bookRepository.save(book);
+            return new ResponseEntity<>(save, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
